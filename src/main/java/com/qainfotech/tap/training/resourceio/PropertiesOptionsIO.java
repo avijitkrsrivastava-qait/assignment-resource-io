8c3e5ab8c10d7c72462db79e12e0376bf8a948b3
Avijit Kumar Srivastava <avijitkumar@qainfotech.com>
2017-06-27 18:03:18 +0530

    Update PropertiesOptionsIO.java

--- src/main/java/com/qainfotech/tap/training/resourceio/PropertiesOptionsIO.java
+++ src/main/java/com/qainfotech/tap/training/resourceio/PropertiesOptionsIO.java
@@ -1,18 +1,36 @@
 package com.qainfotech.tap.training.resourceio;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Properties;
 
 /**
  *
  * @author Ramandeep RamandeepSingh AT QAInfoTech.com
  */
-public class PropertiesOptionsIO{
-    
-    public Object getOptionValue(String optionKey) throws IOException {
-        throw new UnsupportedOperationException("Not implemented.");
+public class PropertiesOptionsIO {
+
+    public String getOptionValue(String optionKey) throws IOException {
+
+        FileReader fileReaderObject = new FileReader(new File("C:\\Users\\avijitkumar\\Documents\\NetBeansProjects\\assignment-resource-io-master\\src\\main\\resources\\options.properties"));
+
+        Properties p = new Properties();
+        p.load(fileReaderObject);
+
+        String s = p.getProperty(optionKey);
+        return s;
+
     }
 
     public void addOption(String optionKey, Object optionValue) throws IOException {
-        throw new UnsupportedOperationException("Not implemented.");
+        FileWriter fileWriterObject = new FileWriter(new File("C:\\Users\\avijitkumar\\Documents\\NetBeansProjects\\assignment-resource-io-master\\src\\main\\resources\\options.properties"), true);
+        Properties p = new Properties();
+        String s = optionValue.toString();
+        p.setProperty(optionKey, s);
+        p.store(fileWriterObject, null);
+        fileWriterObject.close();
+
     }
 }
